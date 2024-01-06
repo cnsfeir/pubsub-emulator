730052a04f8d62bed98cdfa7181666fb7a889509
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-06 17:37:13 -0300

    🐛 Update sync.sh script

--- pubsub_emulator/sync.sh
+++ pubsub_emulator/sync.sh
@@ -3,10 +3,10 @@ $(gcloud beta emulators pubsub env-init)
 gcloud pubsub topics list --format=json > topics.json
 gcloud pubsub subscriptions list --format=json > subscriptions.json
 
-poetry run python sync.py
+poetry run python pubsub_emulator/sync.py
 
 rm topics.json
 rm subscriptions.json
 
-poetry run python main.py fetch-topics
-poetry run python main.py fetch-subscriptions
+poetry run python pubsub_emulator fetch-topics
+poetry run python pubsub_emulator fetch-subscriptions
