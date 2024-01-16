f7ce522618b3453d08ce3347984f687e11c96fa3
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-16 09:52:41 -0300

    🔊 Add logs to the shell script

--- pubsub_emulator/sync.sh
+++ pubsub_emulator/sync.sh
@@ -1,9 +1,15 @@
+echo "\n 🚧 Setting up Pub/Sub emulator"
 $(gcloud beta emulators pubsub env-init)
 
+echo "\n 🔎 Fetching topics"
 gcloud pubsub topics list --format=json > topics.json
+
+echo "\n 🔎 Fetching subscriptions"
 gcloud pubsub subscriptions list --format=json > subscriptions.json
 
+echo "\n 🔄 Syncing..."
 poetry run python pubsub_emulator sync
 
 rm topics.json
 rm subscriptions.json
+echo "\n ✅ Done! \n"
