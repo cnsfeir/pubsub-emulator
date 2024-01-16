65bcbc22b6af996104c17147bc7987ff8d012261
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-16 09:46:13 -0300

    🚚 Move utils functions to a module

---
+++ pubsub_emulator/utils/__init__.py
@@ -0,0 +1,2 @@
+from pubsub_emulator.utils.dictionary import to_snakecase, update_dictionary
+from pubsub_emulator.utils.url import translate_url
--- pubsub_emulator/utils/dictionary.py
+++ pubsub_emulator/utils/dictionary.py
@@ -1,9 +1,4 @@
-import json
-import re
-
 from caseconverter import snakecase
-
-from pubsub_emulator.constants import LOCAL_BASE_URL, SERVICE_MAPPINGS_PATH, SERVICE_NAME_PATTERN
 
 
 def to_snakecase(original: dict) -> dict:
@@ -28,22 +23,3 @@ def update_dictionary(original: dict, new: dict) -> dict:
 
         original[key] = value
     return original
-
-
-def translate_url(url: str) -> str:
-    """
-    Translates the Cloud service URL to their local equivalent.
-    """
-    if not (match := re.search(SERVICE_NAME_PATTERN, url)):
-        raise ValueError(f"Invalid URL {url}")
-
-    service = match.group(1)
-    endpoint = url[len(match.group(0)) :]
-
-    with open(SERVICE_MAPPINGS_PATH, "r", encoding="utf-8") as file:
-        service_mappings = json.load(file)
-
-    if not (port := service_mappings.get(service)):
-        raise ValueError(f"Unknown local URL for service {service}")
-
-    return LOCAL_BASE_URL.format(port=port) + endpoint
