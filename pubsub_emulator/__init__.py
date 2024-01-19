4545c6c4cc58ad1773f2adf8dacc0dd864c69ea7
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-18 21:30:40 -0300

    🔖 Release version 0.2.0

--- pubsub_emulator/__init__.py
+++ pubsub_emulator/__init__.py
@@ -3,4 +3,4 @@ An easy-to-use CLI app for effortlessly managing your local Pub/Sub environment
 and automatically syncing it with your Google Cloud Pub/Sub configuration
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
